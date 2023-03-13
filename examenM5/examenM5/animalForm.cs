d14e690c11731d9fc0c5a2fadeb273b4d4de68fe
Adrian <adrianmr1434@gmail.com>
2023-03-13 09:01:46 +0100

    versio5

---
+++ examenM5/examenM5/animalForm.cs
@@ -0,0 +1,20 @@
+namespace examenM5
+{
+    public partial class animalForm : Form
+    {
+        public animalForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ examenM5/examenM5/viatgeForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace examenM5
+{
+    public partial class viatgeForm : Form
+    {
+        public viatgeForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
